24c3df7213b6a34bf15ef7069751dc13701d5959
baAlhassane <alhassaanebai36@gmail.com>
2024-01-16 00:57:52 +0100

    commited by deleting ctx bean in main

--- src/main/java/com/alhas2024/spring2024/Spring2024Application.java
+++ src/main/java/com/alhas2024/spring2024/Spring2024Application.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Spring2024Application {
 	public static void main(String[] args) {
-		var ctx=SpringApplication.run(Spring2024Application.class, args);
+		SpringApplication.run(Spring2024Application.class, args);
 
-        MyFirstService mysfirsMyFirstService=ctx.getBean(MyFirstService.class);
-        System.out.println( mysfirsMyFirstService.toTellingStory());
 	}
 }
